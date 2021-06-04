9b07edfd18848b271abc17623605099d61a8aa96
Ayush Bansal <bayush611@gmail.com>
2021-06-04 20:11:08 +0530

    Traversing in linked lists.

--- Linked_List/Operations/Traversing.c
+++ Linked_List/Operations/Traversing.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node
-{
+struct Node{
     int data;
     struct Node *next;
 };
 
-void linkedListTraversal(struct Node *ptr)
-{
-    while (ptr != NULL)
-    {
+void linkedListTraversal(struct Node *ptr) {
+    while (ptr != NULL) {
         printf("Element: %d\n", ptr->data);
         ptr = ptr->next;
     }
 }
 
-int main()
-{
+int main() {
     struct Node *head;
     struct Node *second;
     struct Node *third;
