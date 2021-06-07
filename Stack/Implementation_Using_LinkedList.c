e70250733fca2c0e45451bc0a737541241e214db
Ayush Bansal <bayush611@gmail.com>
2021-06-07 18:38:51 +0530

    creating a stack using linked lists

---
+++ Stack/Implementation_Using_LinkedList.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+// Creating a node
+struct Node{
+    int data;
+    struct Node *next;
+};
+
+int main(){
+    struct Node *top = NULL;
+    printf("Stack using Linked List has been created sucessfully");
+}
